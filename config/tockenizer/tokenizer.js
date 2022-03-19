3053409745e1065c4361e9bc30e842e6a11ae883
carlosSploit <arturo14212000@gmail.com>
2022-03-19 01:47:11 -0500

    a 19 03 2022

--- config/tockenizer/tokenizer.js
+++ config/tockenizer/tokenizer.js
@@ -15,6 +15,16 @@ module.exports = function verifyToken(req,res,next){
         const token = listdatetoken[listdatetoken.length-1]
         const payload = jwt.decode(token, config.apidatkey)
         if (payload == null) return res.status(403).send({message: 'No tienes autorizacion'});
+        // coomprobamos la existencia de las key y sus valores dados, entre el json esperado y el json desifrado
+        for (var key in config.apidatkey) {
+            try{
+                if(payload.user[key] != config.apidatkey[key]){
+                    return res.status(403).send({message: 'El toquen es invalido'});
+                }
+            }catch(Exeption){
+                return res.status(403).send({message: 'El formato de la key no conside coon el formato esperado'});
+            }   
+        }
         req.token = token
         next()
      }else{
