e76b86b40468291d0d92d787140cdac1bc1fb43e
carlosSploit <arturo14212000@gmail.com>
2022-03-21 23:59:37 -0500

    a 21 03 2022

--- routes/generic.js
+++ routes/generic.js
@@ -3,7 +3,7 @@ const rooutes = express.Router()
 //######################### rooutes ###################################
 //listar
 rooutes.get('/', async (req, res) => {
-    
+    return res.send({"messege": "La api esta funcionando correctamente"})
 })
 
 module.exports = rooutes
